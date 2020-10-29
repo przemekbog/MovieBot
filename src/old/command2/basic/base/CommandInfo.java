0bbe19b911fb180e6cce1d6f5899815e4b0ede4d
przemek.bog@gmail.com <5600777-przemek_bog@users.noreply.gitlab.com>
2020-10-30 00:33:36 +0100

    Removed files with old code

--- src/old/command2/basic/base/CommandInfo.java
+++
@@ -1,7 +0,0 @@
-package com.pbo.movieBot.command2.basic.base;
-
-public interface CommandInfo {
-    String getSyntaxExample();
-    String getShortInfo();
-    String getDetailedInfo();
-}
--- src/old/command2/basic/base/CommandManager.java
+++
@@ -1,9 +0,0 @@
-package com.pbo.movieBot.command2.basic.base;
-
-import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
-
-public interface CommandManager {
-    void processCommand(GuildMessageReceivedEvent event, String commandWithoutPrefix);
-    void setExternalManager(CommandManager externalManager);
-    void removeExternalManager();
-}
