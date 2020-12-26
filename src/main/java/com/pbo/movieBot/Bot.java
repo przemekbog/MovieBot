ec321c4a0fba9b0225375118d59576161aabf6c4
przemek_bog <5600777-przemek_bog@users.noreply.gitlab.com>
2020-12-26 23:32:14 +0100

    Removed commented out code from the Bot class.

--- src/main/java/com/pbo/movieBot/Bot.java
+++ src/main/java/com/pbo/movieBot/Bot.java
@@ -14,28 +14,6 @@ import javax.security.auth.login.LoginException;
 
 public class Bot {
     public static void main(String[] args) throws LoginException {
-//        IntegerToken token1 = new IntegerToken(5);
-//        IntegerToken token2 = new IntegerToken(10);
-//        TokenTemplate template1 = new TokenTemplate(token1);
-//        TokenTemplate template2 = new TokenTemplate(IntegerToken.class);
-//
-//
-//
-//        System.out.println(template1.matches(token1));
-//        System.out.println(template1.matches(token2));
-//        System.out.println("");
-//        System.out.println(template2.matches(token1));
-//        System.out.println(template2.matches(new StringToken("5")));
-
-//        System.out.println(tokenizer.tokenize("Many things have weird characters, like for example ;'./.,. I like tokenization. 2 + 3 = 5"));
-//        BasicCommandManager commandManager = new BasicCommandManager();
-//        commandManager.addCommands(
-//               new TestCommand(),
-//               new TestComplex()
-//        );
-//
-//        CommandClient commandClient = new CommandClient(commandManager, "!");
-        //
 
         CommandExecutorImpl executor = new CommandExecutorImpl(new Object());
         executor.addCommands(
