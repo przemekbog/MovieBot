a660b4c9abc2f56481a072568dac21c7ac2c20a1
przemek_bog <5600777-przemek_bog@users.noreply.gitlab.com>
2020-12-29 16:59:31 +0100

    Finished HelpCommand

--- src/main/java/com/pbo/movieBot/command/base/CommandHelp.java
+++ src/main/java/com/pbo/movieBot/command/base/CommandHelp.java
@@ -3,7 +3,7 @@ package com.pbo.movieBot.command.base;
 import java.util.List;
 
 public interface CommandHelp {
-    String getSyntax();
+    List<String> getSyntaxList();
     String getShortInfo();
     String getDetailedInfo();
     List<String> getUseExamples();
--- src/main/java/com/pbo/movieBot/commands/help/HelpCommand.java
+++ src/main/java/com/pbo/movieBot/commands/help/HelpCommand.java
@@ -3,14 +3,17 @@ package com.pbo.movieBot.commands.help;
 import com.pbo.movieBot.command.base.Command;
 import com.pbo.movieBot.command.base.CommandEvent;
 import com.pbo.movieBot.command.base.CommandExecutor;
+import com.pbo.movieBot.command.base.CommandHelp;
 import com.pbo.movieBot.emoji.Emoji;
-import net.dv8tion.jda.api.entities.Emote;
-import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.MessageBuilder;
+import net.dv8tion.jda.api.entities.*;
 
+import java.awt.*;
 import java.util.*;
+import java.util.List;
 
 public class HelpCommand extends Command {
-    private static final Character DOT_CHARACTER = '\u2022';
     private static final String OK_HAND = Emoji.OK_HAND.getText();
 
     @Override
@@ -19,8 +22,13 @@ public class HelpCommand extends Command {
     }
 
     @Override
+    public CommandHelp getHelp() {
+        return new HelpCommandHelp();
+    }
+
+    @Override
     public void execute(CommandEvent event, Object context) {
-        String message = "";
+        Message message;
 
         if(event.getArgs().equals("")) {
             message = getAllCommandInfo(event);
@@ -28,54 +36,38 @@ public class HelpCommand extends Command {
             message = getDetailedInfo(event);
         }
 
-        sendPrivateMessage(event, message);
+        sendPrivateMessage(event.getAuthor(), message);
         event.getMessage().addReaction(OK_HAND).queue();
     }
 
-    private String getAllCommandInfo(CommandEvent event) {
+    private Message getAllCommandInfo(CommandEvent event) {
         CommandExecutor executor = event.getCommandExecutor();
-        List<Command> commands = sortCommands(executor.getCommands());
-        StringBuilder builder = new StringBuilder(getBigHelpText());
+        return CommandListMessageCreator.getCommandListMessage(executor.getCommands());
+    }
 
-        for(Command command : commands) {
-            builder.append(getShortHelpFromCommand(command));
+    private Message getDetailedInfo(CommandEvent event) {
+        List<Command> commands = event.getCommandExecutor().getCommands();
+        String name = event.getArgs();
+        Optional<Command> commandOptional = findCommandWithName(commands, name);
+
+        if(!commandOptional.isPresent()) {
+            return new MessageBuilder().append("Command not found " + Emoji.DISAPPOINTED_FACE.getText()).build();
         }
 
-        return builder.toString();
+        Command command = commandOptional.get();
+        return DetailedHelpMessageCreator.getDetailedInfoFromCommand(command);
     }
 
-    private String getDetailedInfo(CommandEvent event) {
-        MessageChannel channel = event.getChannel();
-        // TODO: Implement
-
-        throw new IllegalStateException("Not implemented yet");
+    private void sendPrivateMessage(User user, Message message) {
+        user.openPrivateChannel().queue(privateChannel ->
+                privateChannel.sendMessage(message).queue()
+        );
     }
 
-    private List<Command> sortCommands(List<Command> commands) {
-        ArrayList<Command> copy = new ArrayList<>(commands);
-        copy.sort(Comparator.comparing(Command::getName));
-        return copy;
-    }
-
-    private String getShortHelpFromCommand(Command command) {
-        String commandName = command.getName();
-        String shortInfo = command.getHelp().getShortInfo();
-        return DOT_CHARACTER + "  `" + commandName + "` - " + shortInfo + System.lineSeparator();
-    }
-
-    private void sendPrivateMessage(CommandEvent event, String message) {
-        event.getAuthor().openPrivateChannel().queue(privateChannel -> {
-            privateChannel.sendMessage(message).queue();
-        });
-    }
-
-    private String getBigHelpText() {
-        return  "```" +
-                " _____ _____ __    _____ \n" +
-                "|  |  |   __|  |  |  _  |\n" +
-                "|     |   __|  |__|   __|\n" +
-                "|__|__|_____|_____|__|   " +
-                "```";
+    private Optional<Command> findCommandWithName(List<Command> commands, String name) {
+        return commands.stream()
+                .filter(command -> command.getName().equals(name))
+                .findAny();
     }
 
 }
