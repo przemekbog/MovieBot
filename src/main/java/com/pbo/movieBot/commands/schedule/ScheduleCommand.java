892b89927a1e9630e6cd761920bb6147332d1a48
przemek_bog <5600777-przemek_bog@users.noreply.gitlab.com>
2021-01-13 15:14:10 +0100

    Added basic functionality to ScheduleCommand

--- src/main/java/com/pbo/movieBot/commands/schedule/ScheduleCommand.java
+++ src/main/java/com/pbo/movieBot/commands/schedule/ScheduleCommand.java
@@ -3,6 +3,8 @@ package com.pbo.movieBot.commands.schedule;
 import com.pbo.movieBot.command.base.Command;
 import com.pbo.movieBot.command.base.CommandEvent;
 import com.pbo.movieBot.commands.MovieBotContext;
+import com.pbo.movieBot.commands.schedule.nlp.SchedulingPipeline;
+import com.pbo.movieBot.movieSaving.base.MovieReservation;
 
 import java.util.List;
 
@@ -15,6 +17,16 @@ public class ScheduleCommand extends Command<MovieBotContext> {
 
     @Override
     public void execute(CommandEvent event, MovieBotContext context) {
-        event.getChannel().sendMessage(".").queue();
+        try {
+            SchedulingPipeline pipeline = new SchedulingPipeline();
+            MovieReservation reservation = pipeline.process(event.getArgs());
+            event.getChannel().sendMessage(reservation.toString()).queue();
+        } catch(IllegalArgumentException e) {
+            sendIncorrectInputMessage(event);
+        }
+    }
+
+    private void sendIncorrectInputMessage(CommandEvent event) {
+        event.getChannel().sendMessage("You fucked something up, m8").queue();
     }
 }
