d0860885cf7cd4a4295b8c8c65aab457426a2a5a
przemek_bog <5600777-przemek_bog@users.noreply.gitlab.com>
2021-01-25 16:24:34 +0100

    Modified the CommandListMessageCreator class

--- src/main/java/com/pbo/movieBot/bot/commands/help/CommandListMessageCreator.java
+++ src/main/java/com/pbo/movieBot/bot/commands/help/CommandListMessageCreator.java
@@ -1,32 +1,40 @@
 package com.pbo.movieBot.bot.commands.help;
 
 import com.pbo.movieBot.command.base.Command;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.MessageBuilder;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
 public class CommandListMessageCreator {
-    private static final Character DOT_CHARACTER = '\u2022';
 
     public static Message getCommandListMessage(List<Command> commands) {
-        List<Command> sortedCommands = sortCommands(commands);
         MessageBuilder builder = new MessageBuilder();
-
-        builder.appendCodeBlock(getBigHelpText(), "txt");
-        for(Command command : sortedCommands) {
-            builder.append(getShortHelpFromCommand(command));
-        }
+        builder.setEmbed(getCommandListEmbed(commands));
 
         return builder.build();
     }
 
-    private static String getShortHelpFromCommand(Command command) {
-        String commandName = command.getName();
-        String shortInfo = command.getHelp().getShortInfo();
-        return DOT_CHARACTER + "  `" + commandName + "` - " + shortInfo + System.lineSeparator();
+    private static MessageEmbed getCommandListEmbed(List<Command> commands) {
+        List<Command> sortedCommands = sortCommands(commands);
+        EmbedBuilder builder = new EmbedBuilder();
+
+        String codeBlock = String.format("""
+                ```
+                %s
+                ```
+                """, getBigHelpText());
+
+        builder.setDescription(getBigHelpText());
+        for(Command command : sortedCommands) {
+            builder.addField(command.getName(), command.getHelp().getShortInfo(), false);
+        }
+
+        return builder.build();
     }
 
     private static List<Command> sortCommands(List<Command> commands) {
@@ -36,9 +44,13 @@ public class CommandListMessageCreator {
     }
 
     private static String getBigHelpText() {
-        return  " _____ _____ __    _____ \n" +
-                "|  |  |   __|  |  |  _  |\n" +
-                "|     |   __|  |__|   __|\n" +
-                "|__|__|_____|_____|__|   ";
+        return """
+                ```
+                 _____ _____ __    _____
+                |  |  |   __|  |  |  _  |
+                |     |   __|  |__|   __|
+                |__|__|_____|_____|__|                                  \
+                ```
+                """; // Backslash is there on purpose. Do not remove!
     }
 }
