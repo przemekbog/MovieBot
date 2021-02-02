e71f99063968acfe3a127cc119e970ab87f68188
przemek_bog <5600777-przemek_bog@users.noreply.gitlab.com>
2021-02-02 23:52:14 +0100

    Added basic logging

--- src/main/java/com/pbo/movieBot/command/CommandExecutorImpl.java
+++ src/main/java/com/pbo/movieBot/command/CommandExecutorImpl.java
@@ -4,6 +4,8 @@ import com.pbo.movieBot.command.base.Command;
 import com.pbo.movieBot.command.base.CommandEvent;
 import com.pbo.movieBot.command.base.CommandExecutor;
 import net.dv8tion.jda.api.entities.MessageChannel;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -11,6 +13,7 @@ import java.util.List;
 import java.util.Map;
 
 public class CommandExecutorImpl<T> implements CommandExecutor {
+    private Log log = LogFactory.getLog(CommandExecutorImpl.class);
     private Map<String, Command<T>> commandMap = new HashMap<>();
     private T context;
 
@@ -22,11 +25,14 @@ public class CommandExecutorImpl<T> implements CommandExecutor {
     public void execute(CommandEvent event) {
         String name = event.getCommandName();
 
+        logCommandExecutionAttempt(event);
         if(commandMap.containsKey(name)) {
             Command<T> command = commandMap.get(name);
+            logCommandFound(event);
             command.execute(event, context);
         } else {
-            sendCommandNotFoundMessage(event.getChannel());
+            logCommandNotFound(event);
+            sendCommandNotFoundMessage(event);
         }
     }
 
@@ -47,8 +53,27 @@ public class CommandExecutorImpl<T> implements CommandExecutor {
         addByAliases(command);
     }
 
-    private void sendCommandNotFoundMessage(MessageChannel channel) {
-        channel.sendMessage("Command not found :(").queue();
+    private void logCommandExecutionAttempt(CommandEvent event) {
+        String authorsNick = event.getAuthor().getAsTag();
+        String logMessage = String.format(
+                "%s tried issuing \"%s\" command with arguments \"%s\"",
+                authorsNick, event.getCommandName(), event.getArgs());
+
+        log.info(logMessage);
+    }
+
+    private void logCommandFound(CommandEvent event) {
+        String logMessage = String.format("Executing \"%s\" command...", event.getCommandName());
+        log.info(logMessage);
+    }
+
+    private void logCommandNotFound(CommandEvent event) {
+        String logMessage = String.format("Could not find the \"%s\" command.", event.getCommandName());
+        log.error(logMessage);
+    }
+
+    private void sendCommandNotFoundMessage(CommandEvent event) {
+        event.getChannel().sendMessage("Command not found :(").queue();
     }
 
     private void addByAliases(Command<T> command) {
